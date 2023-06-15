e7714e799a1c58cc44a9896cc52869dbb1ea5434
Josef Hardi <josef.hardi@gmail.com>
2023-06-14 17:31:57 -0700

    Show the repair menu if necessary

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,35 +1,42 @@
+import { useContext } from 'react';
 import { AppBar, Box, Toolbar, Typography, Container } from '@mui/material';
 import BuildCircleIcon from '@mui/icons-material/BuildCircle';
+import AppContext from '../../pages/AppContext';
 import CompletenessErrorNavMenu from '../NavMenu/CompletenessErrorNavMenu';
 import AdherenceErrorNavMenu from '../NavMenu/AdherenceErrorNavMenu';
 import OverviewNavMenu from '../NavMenu/OverviewNavMenu';
+import { hasCompletenessErrors, hasAdherenceErrors } from '../../helpers/app-utils';
 
-const Navbar = () => (
-  <AppBar component="nav" position="sticky">
-    <Container maxWidth="xl">
-      <Toolbar disableGutters>
-        <BuildCircleIcon sx={{ paddingRight: 1 }} />
-        <Typography
-          variant="h6"
-          noWrap
-          sx={{
-            mr: 2,
-            display: { xs: 'none', md: 'flex' },
-            color: 'inherit',
-            textDecoration: 'none',
-          }}
-        >
-          Metadata Spreadsheet Validator
-        </Typography>
-        <Box sx={{ flexGrow: 1 }} />
-        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-          <OverviewNavMenu />
-          <CompletenessErrorNavMenu />
-          <AdherenceErrorNavMenu />
-        </Box>
-      </Toolbar>
-    </Container>
-  </AppBar>
-);
+const Navbar = () => {
+  const { appData } = useContext(AppContext);
+  const { reporting } = appData;
+  return (
+    <AppBar component="nav" position="sticky">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <BuildCircleIcon sx={{ paddingRight: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Metadata Spreadsheet Validator
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <OverviewNavMenu />
+            {hasCompletenessErrors(reporting) && <CompletenessErrorNavMenu />}
+            {hasAdherenceErrors(reporting) && <AdherenceErrorNavMenu />}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 
 export default Navbar;
