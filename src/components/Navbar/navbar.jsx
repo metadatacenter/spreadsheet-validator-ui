bdcbe25f65894934d8cf15865cc9affb295e3aa8
Josef Hardi <josef.hardi@gmail.com>
2023-06-26 12:14:01 -0700

    Show the instruction banner when errors not found

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,15 +1,37 @@
-import { useContext } from 'react';
-import { AppBar, Box, Toolbar, Typography, Container } from '@mui/material';
+import { useContext, useState, useEffect } from 'react';
+import { AppBar, Box, Toolbar, Typography, Container, Link, styled } from '@mui/material';
 import BuildCircleIcon from '@mui/icons-material/BuildCircle';
 import AppContext from '../../pages/AppContext';
 import CompletenessErrorNavMenu from '../NavMenu/CompletenessErrorNavMenu';
 import AdherenceErrorNavMenu from '../NavMenu/AdherenceErrorNavMenu';
 import OverviewNavMenu from '../NavMenu/OverviewNavMenu';
-import { hasCompletenessErrors, hasAdherenceErrors } from '../../helpers/app-utils';
+import { hasCompletenessErrors, hasAdherenceErrors, isRepairCompleted } from '../../helpers/app-utils';
+import { BLACK, GREEN } from '../../constants/Color';
+
+const Banner = styled(Box)({
+  backgroundColor: GREEN,
+  color: BLACK,
+  padding: '10px',
+  textAlign: 'center',
+});
 
 const Navbar = () => {
-  const { appData } = useContext(AppContext);
+  const [hide, setHide] = useState(true);
+  const { appData, patches } = useContext(AppContext);
   const { reporting } = appData;
+  const noErrorFound = reporting.length === 0;
+
+  useEffect(
+    () => {
+      if (isRepairCompleted(reporting, patches)) {
+        setHide(false);
+      } else {
+        setHide(true);
+      }
+    },
+    [reporting, patches],
+  );
+
   return (
     <AppBar component="nav" position="sticky">
       <Container maxWidth="xl">
@@ -35,6 +57,23 @@ const Navbar = () => {
           </Box>
         </Toolbar>
       </Container>
+      <Banner hidden={hide}>
+        No errors found.&nbsp;
+        {noErrorFound ? (
+          'Please proceed to upload the spreadsheet to the'
+        ) : (
+          'You can download the repaired spreadsheet now and proceed to upload it to the'
+        )}
+        &nbsp;
+        <Link
+          href="http://ingest.hubmapconsortium.org"
+          rel="noopener"
+          target="_blank"
+        >
+          HuBMAP ingest portal
+        </Link>
+        .
+      </Banner>
     </AppBar>
   );
 };
