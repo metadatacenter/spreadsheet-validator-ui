9a662867dc9586be6854888d719eeffc7a5d06e5
Josef Hardi <josef.hardi@gmail.com>
2023-08-23 10:56:55 -0700

    Insert Google Analytics measurement id

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import ReactGA from 'react-ga';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+ReactGA.initialize('G-VD65FRLG0Q');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
