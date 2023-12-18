e4effff43b0c6f2c75905ba07d3aad8ff7c96475
Josef Hardi <josef.hardi@gmail.com>
2023-12-18 13:45:33 -0600

    Update to v1.3.4

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ const LandingPageContainer = () => (
       >
         Metadata Spreadsheet
       </Button>
-      <Button component="div" disabled>UI Version: 1.3.3</Button>
+      <Button component="div" disabled>UI Version: 1.3.4</Button>
     </Stack>
     <Outlet />
   </Stack>
