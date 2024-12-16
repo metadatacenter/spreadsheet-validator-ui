c89e0f65b3b0270c7fb8089a8a04ed84dd4b563a
Josef Hardi <josef.hardi@gmail.com>
2024-12-16 13:05:23 -0800

    Fix the missing CEDAR icon

--- src/components/PageFooter/page-footer.jsx
+++ src/components/PageFooter/page-footer.jsx
@@ -12,24 +12,24 @@ const PageFooter = () => (
       justifyContent: 'center',
     }}
   >
-    <Block sx={{ paddingRight: '40px' }}>
+    <Block>
       <a href="https://metadatacenter.org/" target="_blank" rel="noreferrer">
         <img
-          src="https://more.metadatacenter.org/sites/default/files/cedar_logo3.png"
+          src="https://metadatacenter.org/wp-content/uploads/2019/03/CEDAR_logo_tree_no_line-3-80x80.png"
           alt="CEDAR logo"
-          width="400px"
+          width="70px"
         />
       </a>
     </Block>
-    <Block sx={{ width: '35%' }}>
-      Copyright @ 2023 The Board of Trustees of Leland Stanford Junior University
+    <Block sx={{ width: '38%', textAlign: 'center' }}>
+      Copyright © 2023-2025 The Board of Trustees of Leland Stanford Junior University
     </Block>
     <Block>
       <a href="https://github.com/metadatacenter/spreadsheet-validator-ui" target="_blank" rel="noreferrer">
         <img
           src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
           alt="GitHub logo"
-          width="40px"
+          width="60px"
         />
       </a>
     </Block>
